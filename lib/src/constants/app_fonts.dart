2d5b23a1905ce22ea547e64ca69134a28386b56e
sonam-2011 <sonamrastogi.20nov@gmail.com>
2023-04-27 19:25:01 +0530

    initial commit

---
+++ lib/src/constants/app_fonts.dart
@@ -0,0 +1,5 @@
+class Fonts{
+  static const robotoLight = "Roboto-Light";
+  static const robotoMedium = "Roboto-Medium";
+  static const robotoRegular = "Roboto-Regular";
+}
---
+++ lib/src/constants/app_strings.dart
@@ -0,0 +1,31 @@
+class AppStrings {
+  static const String rupeeSymbol = "₹";
+  static const String unknownError =
+      'Unknown error! Please try again after some time.';
+  static const String connectionTimeout =
+      'Connection timeout. Please try again after some time.';
+  static const String noConnection =
+      'No connection. Please turn on your internet!';
+  static const String unAuthorize = 'Unauthorized. Please login!';
+  static const String error = 'Error';
+  static const discover = 'Home';
+  static const nearby = 'Favourites';
+  static const appointment = 'chat';
+  static const profile = 'Profile';
+  static const fieldCantBeEmpty = 'Field cant be empty';
+  static const enterVaildName = 'Enter a valid name ';
+  static const emailCantBeEmpty = 'Email cant be empty';
+  static const enterValidEmail = 'Enter valid email';
+  static const numberCantBeEmpty = 'Number cant be empty';
+  static const enterValidNumber = 'Enter valid number';
+  static const passwordCantBeEmpty = 'Password cant be empty';
+  static const confirmPasswordCantBeEmpty = 'Confirm Password cant be empty';
+  static const passwordValidation = 'Password length should be greater then 6';
+  static const confirmPasswordValidation = 'password do not match';
+  static const categoryType = 'category';
+  static const terms = 'Agree Terms & Conditions*';
+  static const otpValidation = 'OTP not validate';
+  static const aboutUs = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. \n Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.';
+  static const welcomeText = 'We welcome you online student to our association and provide you the new curriculum details for your best studies';
+  static const description = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.';
+}
